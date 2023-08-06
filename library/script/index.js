1c59a9bb8b6d1b38d34c5daad9a4920bfb1dc078
Gennady <8581807@mail.ru>
2023-08-06 09:13:40 +0300

    feat: create burger and media tablet 768

--- library/script/index.js
+++ library/script/index.js
@@ -1,1 +1,25 @@
 console.log("Вёрстка валидная +10\nВёрстка семантическая +16\nВёрстка соответствует макету +54\nОбщие требования к верстке +20\n")
+
+let burger = document.querySelector(".burger");
+let close = document.querySelector(".close-burger");
+let nav = document.querySelector(".head-container nav");
+let menu = document.querySelector(".menu-ul");
+let menuLi = document.querySelector(".menu-ul li");
+let mobileIcons = document.querySelector(".mobile-icons");
+let displayUl = document.querySelector(".head-container ul")
+
+
+burger.addEventListener('click', function(){
+    nav.classList.toggle('mobile-nav');
+    menu.classList.toggle('burger-ul');
+    menuLi.classList.toggle('burger-li');
+    mobileIcons.classList.toggle('burger-icons');
+    nav.classList.toggle('animation');
+})
+
+close.addEventListener('click', function(){
+    nav.classList.toggle('mobile-nav');
+    menu.classList.toggle('burger-ul');
+    menuLi.classList.toggle('.burger-li');
+    mobileIcons.classList.toggle('burger-icons');
+})
